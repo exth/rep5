287b0483f75727ce2edd8f5e0b7b9856e08995f3
clxwly <lxwlyyy@gmail.com>
2026-04-13 06:26:01 +0300

    My 1st work commit

---
+++ gitTestRep5/gitTestRep5/MyFolder/MyTextView.swift
@@ -0,0 +1,19 @@
+//
+//  MyTextView.swift
+//  gitTestRep5
+//
+//  Created by Вадим on 13.04.26.
+//
+
+import SwiftUI
+
+struct MyTextView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("asadwed test view")
+    }
+}
+
+#Preview {
+    MyTextView()
+}
