9db346d7bdf1aef4e2469dcfccc6e14ecf8879c5
clxwly <lxwlyyy@gmail.com>
2026-04-13 06:20:46 +0300

    1.Initial commit

---
+++ gitTestRep5/gitTestRep5/gitTestRep5App.swift
@@ -0,0 +1,17 @@
+//
+//  gitTestRep5App.swift
+//  gitTestRep5
+//
+//  Created by Вадим on 13.04.26.
+//
+
+import SwiftUI
+
+@main
+struct gitTestRep5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
